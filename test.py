ce64640c80feafa26bc76863afb45f404d9a0145
Andre Machado <csantos.machado@gmail.com>
2019-09-23 20:33:30 -0300

    Update test.py

--- test.py
+++ test.py
@@ -135,17 +135,15 @@ class AssessmentTestCases(unittest.TestCase):
         confirm_elem = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "SubmitButton--complete")))
         confirm_elem.click()
 
-        try:
-            session_id_elem = wait.until(
-                EC.presence_of_element_located((By.ID, "sessionId"))
-            )
-        except:
-            print(self.driver.get_screenshot_as_base64())
+        session_id_elem = wait.until(
+            EC.presence_of_element_located((By.ID, "sessionId"))
+        )
                         
         self.assertIn("order_success.html", self.driver.current_url)
         self.assertTrue(session_id_elem.text)
 
     def tearDown(self):
+        print(self.driver.get_screenshot_as_base64())
         self.driver.close()
 
 
